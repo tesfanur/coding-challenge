2802f9713a3fd5f985023ed0f04cdc1137bddb9f
Tesfaye Belachew <get.tesfaye.belachew@gmail.com>
2018-06-04 19:57:51 +0300

    full stack bootcamp coding challenge questions

---
+++ coding-excercise-one.js
@@ -0,0 +1,33 @@
+/**
+ * Author: Tesfaye Belachew, Email: get.tesfaye.belachew@gmail.com
+ * 
+ * Given an array of integers, find if the array contains any duplicates. 
+ * Your function should return true if any value appears at least twice in the array, 
+ * and it should return false if every element is distinct. *
+ */
+//start time: 6:51 AM -  6:55 AM, 7:20 AM -  7:23 AM => 7min
+
+let duplicationExists = (arrayInput) => {
+    //loop through each array element and compare each element 
+    //with the remaining array elements
+    //and exit the loop by returning true if identical element exists
+    for (let i = 0; i < arrayInput.length; i++) {
+        for (let j = i + 1; j < arrayInput.length; j++) {
+            if (arrayInput[i] === arrayInput[j]) {
+                return console.log(true);
+            }
+        }
+    }
+    //otherwise, if no identical element found, return false
+    return console.log(false);
+}
+/**
+ * Test duplcateExists function
+ */
+//returns flase
+duplicationExists([1, 2, 3, 4, 5]);
+//returns true
+duplicationExists([1, 2, 5, 3, 4, 5]);
+//returns true
+duplicationExists([10, 2, 5, 3, 4, , 10, 5, 10]);
+//TODO write unit test for this function
---
+++ coding-excercise-two.js
@@ -0,0 +1,51 @@
+/**
+ * Author: Tesfaye Belachew, Email: get.tesfaye.belachew@gmail.com
+ * 
+ * Optional Question - Find the consecutive subarray within 
+ * an array (containing at least one number) which has the largest product. 
+ * For example, given the array [2, 3, -2, 4],the contiguous subarray [2 ,3] 
+ * has the largest product = 6.
+ */
+//Solution function for the question above
+
+let consecMaxProduct = (arrayInput) => {
+    //declare a variable to hold product of consecutive elements of the arrayInput 
+    let maxProduct;
+    //check array length and return a proper 
+    //message for a user
+    if (arrayInput.length == 0)
+        return console.log("the array is empty");
+    if (arrayInput.length == 1)
+        return console.log("the array has only one element");
+
+    //loop through the array elements
+    for (let i = 0; i < arrayInput.length; i++) {
+        //initialize maxProduct variable with the first consecutive array elements product
+        if (i <= 1)
+            maxProduct = arrayInput[0] * arrayInput[1];
+        //then compare the initial product with the remaining consecutive array elements product
+        //and assign the max value to maxProduct variable
+        if (maxProduct < arrayInput[i] * arrayInput[i + 1] && i + 1 < arrayInput.length)
+            maxProduct = arrayInput[i] * arrayInput[i + 1];
+
+    }
+    //return maxProduct;
+    return console.log(maxProduct);
+}
+/**
+ * Test consecMaxProduct function
+ */
+//returns 200
+consecMaxProduct([100, 2, 3, 4, 5]);
+//returns 150
+consecMaxProduct([1, 2, 50, 3, 4, 5]);
+//returns 160
+consecMaxProduct([10, 2, 5, 30, 4, 16, 10, -50, 10]);
+//returns 6
+consecMaxProduct([2, 3, -2, 4]);
+//returns "the array is empty"
+consecMaxProduct([]);
+//returns "the array has only one element"
+consecMaxProduct([2]);
+//returns 30
+consecMaxProduct([2, 15]);
